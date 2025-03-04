b0a45b16cb5afd95f6a694f23b9d1f94cae1faf5
Yair Chuchem <yairchu@gmail.com>
2025-03-04 22:00:44 +0200

    PartialCovar stores sub_covar instead of real component with redundant entries

--- simprob/partial_covar.py
+++ simprob/partial_covar.py
@@ -16,44 +16,51 @@ class PartialCovar:
     Covariance matrix extended to support uniform distributions variance.
 
     Components:
-    * real: The real component of the full covariance matrix (removing infinite terms)
-    * bounded_subspace: The sub-space that has a bounded distribution
+    * [S, N] bounded_subspace: The sub-space that has a bounded distribution
+    * [S, S] sub_covar: The covariance of the bounded subspace
     """
 
-    real: np.ndarray
     bounded_subspace: np.ndarray = dataclasses.field(kw_only=True)
+    sub_covar: np.ndarray = dataclasses.field(kw_only=True)
 
     def __post_init__(self):
-        self.real = self.assure_symmetric_matrix(self.real)
-        assert self.bounded_subspace.shape[1] == self.dim, (
-            f"{self.bounded_subspace.shape=} {self.dim=}"
-        )
-
-    @property
-    def shape(self):
-        return self.real.shape
+        self.sub_covar = self.assure_symmetric_matrix(self.sub_covar)
 
     @property
     def dim(self):
-        return self.shape[0]
+        return self.bounded_subspace.shape[1]
+
+    @property
+    def shape(self):
+        return (self.dim,) * 2
 
     @property
     def uniform_subspace(self):
         return scipy.linalg.null_space(self.bounded_subspace).T
+
+    @property
+    def real(self):
+        """
+        Real component of the covariance projected to the full covariance matrix
+        (uniform parts are zeroed).
+        """
+        return PartialCovar.transform(self.sub_covar, self.bounded_subspace.T)
 
     def emulate_as_real(self, inf_const=1e6):
         """
         Create a real matrix "emulation" of the partial covariance
         by representing uniform variance as a very large variance.
         """
-        return inf_const * self.uniform_subspace.T @ self.uniform_subspace + self.real
+        return self.real + inf_const * self.uniform_subspace.T @ self.uniform_subspace
 
     def simplify(self):
         return self.real if self.bounded_subspace.shape == self.shape else self
 
     @classmethod
     def uniform(cls, dim):
-        return cls(np.zeros([dim, dim]), bounded_subspace=np.zeros([0, dim])).simplify()
+        return cls(
+            bounded_subspace=np.zeros([0, dim]), sub_covar=np.zeros([0, 0])
+        ).simplify()
 
     @classmethod
     def concat(cls, covars):
@@ -73,8 +80,10 @@ class PartialCovar:
             for dims, s in zip(cum_dims, spaces)
         ]
         return cls(
-            scipy.linalg.block_diag(*[x.real for x in covars]),
             bounded_subspace=np.concatenate(projected_spaces),
+            sub_covar=scipy.linalg.block_diag(
+                *[x if isinstance(x, np.ndarray) else x.sub_covar for x in covars]
+            ),
         ).simplify()
 
     @classmethod
@@ -84,17 +93,15 @@ class PartialCovar:
             if isinstance(mat, np.ndarray)
             else mat.bounded_subspace
         )
-        bounded_part = cls.transform(mat.real, bounded_subspace)
-        delta_space = scipy.linalg.null_space(bounded_part)
-        if delta_space.shape[1] == 0:
-            inv_part = np.linalg.inv(bounded_part)
+        sub = mat if isinstance(mat, np.ndarray) else mat.sub_covar
+        delta_space = scipy.linalg.null_space(sub).T
+        if delta_space.shape[0] == 0:
+            inv_part = np.linalg.inv(sub)
         else:
-            var_space = scipy.linalg.null_space(delta_space.T)
+            var_space = scipy.linalg.null_space(delta_space).T
             inv_part = cls(
-                cls.transform(
-                    np.linalg.inv(cls.transform(bounded_part, var_space.T)), var_space
-                ),
-                bounded_subspace=var_space.T,
+                bounded_subspace=var_space,
+                sub_covar=np.linalg.inv(cls.transform(sub, var_space)),
             )
         return cls.transform(inv_part, bounded_subspace.T)
 
@@ -103,19 +110,20 @@ class PartialCovar:
         real = mat @ covar.real @ mat.T
         if isinstance(covar, np.ndarray):
             return real
-
+        subspace = scipy.linalg.null_space(covar.uniform_subspace @ mat.T).T
         return type(covar)(
-            real,
-            bounded_subspace=scipy.linalg.null_space(covar.uniform_subspace @ mat.T).T,
+            bounded_subspace=subspace,
+            sub_covar=PartialCovar.transform(real, subspace),
         )
 
     def __add__(self, other):
         uni_spaces = [
             x.uniform_subspace for x in [self, other] if not isinstance(x, np.ndarray)
         ]
+        subspace = scipy.linalg.null_space(np.concatenate(uni_spaces)).T
         return type(self)(
-            self.real + other.real,
-            bounded_subspace=scipy.linalg.null_space(np.concatenate(uni_spaces)).T,
+            bounded_subspace=subspace,
+            sub_covar=PartialCovar.transform(self.real + other.real, subspace),
         ).simplify()
 
     __array_priority__ = 1000  # Ensures NumPy prefers calling our roperators
@@ -128,7 +136,10 @@ class PartialCovar:
         "Enforce matrix symmetry (for covariance matrices)"
         if isinstance(mat, PartialCovar):
             return dataclasses.replace(
-                mat, real=PartialCovar.assure_symmetric_matrix(mat.real, do_assert)
+                mat,
+                sub_covar=PartialCovar.assure_symmetric_matrix(
+                    mat.sub_covar, do_assert
+                ),
             )
         res = 0.5 * (mat + mat.T)
         if do_assert:
